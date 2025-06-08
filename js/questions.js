45bd99e222528b1592b496e12ef3fcd1c4f486e6
vesrntechnologies <vennjoy@gmail.com>
2025-06-08 08:34:03 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,639 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which test is conducted to locate the faults in U.G. cables?",
+   
+    answer: "Loop Test",
+    options: [
+      "Loop Test",
+      "External growler Test",
+      "Breakdown Voltage Test",
+      "Insulation Resistance Test"
+    ]
+  },
+     {
+    numb: 2,
+    question: "What does the number 1.40 represent if a stranded conductor is designated as 7/1.40?",
+    answer: "Diameter of each conductor",
+    options: [
+      "Area of cross section",
+      "Radius of one conductor",
+      "Diameter of all conductor", 
+      "Diameter of each conductor"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the size of the neutral core of a 90 sq mm 31/2 core U.G cable?",
+    
+    answer: "45 Sq mm",
+    options: [
+      "45 Sq mm",
+      "50 Sq mm",
+      "70 Sq mm",
+      "90 Sq mm"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the name of the tool as shown in the figure?",
+    immg:"img4",
+    answer: "Crimping tool",
+    options: [
+      "cutting plier",
+      "Wire stripper",
+      "Crimping tool",
+      "Side cutting plier"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which type of joint is used in over head lines for high tensile strength?",
+    answer: "Western union joint",
+    options: [
+      "Scarfed joint",
+      "Married Joint",
+      "Western union joint",
+      "Rat tail joint"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What is the unit for quantity of electricity?",
+    answer: "Coulomb",
+    options: [
+      "Mho",
+      "Coulomb",
+      "Volt/second",
+      "Ampere/second"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What is the name of the soldering method as shown in the figure? ",
+    immg:"img7",
+    answer: "Soldering gun",
+    options: [
+      "Dip Soldering",
+      "Soldering iron",
+      "Soldering gun",
+      "soldering with flame"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What is the name of the soldering method as shown in the figure?",
+    immg:"img8",
+    answer: "Temperature controlled soldering",
+    options: [
+      "Dip Soldering",
+      "Soldering with blow lamp",
+      "Soldering with soldering gun",
+      "Temperature controlled soldering"
+    ]
+  },
+      {
+    numb: 9,
+    question: "How many electrons are there in the third shell of the copper atom?",
+    
+    answer: "18",
+ 
+    options: [
+      "8",
+      "13",
+      "18",
+      "29"
+    ]
+  },
+      {
+    numb: 10,
+    question: "What is the formula for the Quantity of electricity (Q)?",
+ 
+    answer: "Current x Time",
+    options: [
+      "Current x Time",
+      "Voltage x Current",
+      "Current x Resistance",
+      "Voltage x Resistance"
+    ]
+  },
+     {
+    numb: 11,
+    question: "Which formula is used to calculate the power of a DC circuit?",
+  
+    answer: "Current x voltage",
+    options: [
+      "Voltage x time",
+      "Current x voltage",
+      "Current x resistance",
+      "Voltage x resistance"
+    ]
+  },
+     {
+    numb: 12,
+    question: "What is the advantage of the stranded conductor over the solid conductor?",
+    
+    answer: "More flexible",
+    options: [
+      "cost is less",
+      "More flexible",
+      "less voltage drop",
+      "more insulation resistance"
+    ]
+  },
+ {
+    numb: 13,
+    question: "What is the advantage of the crimping terminations?",
+ 
+    answer: "Avoid loose connections",
+    options: [
+      "Increase the voltage",
+      "Reduce the load current",
+      "Avoid loose connections",
+      "Easy to replace"
+    ]
+  },
+{
+    numb: 14,
+    question: "Which part of the underground cable is protecting the metallic sheath against corrosion?",
+    
+    answer: "Bedding",
+  
+    options: [
+      "Serving",
+      "Bedding",
+      "Armouring",
+      "Lead Sheath"
+    ]
+  },
+    {
+    numb: 15,
+    question: "What is the use of dipsoldering method?",
+  
+    answer: "Soldering miniature components on PCB",
+    options: [
+      "Hard soldering",
+      "Piping and cable soldering work",
+      "Soldering miniature components on PCB",
+     "soldering of tin sheets"
+    ]
+  },
+    {
+    numb: 16,
+    question: "Which is the semiconductor material?",
+    
+    answer: "Germanium",
+    options: [
+      "Eureka",
+      "Ebonite",
+      "Maganin",
+      "Germanium"
+    ]
+  },
+    {
+    numb: 17,
+    question: "What is the reading of the micrometer as shown in the figure?",
+      immg:"img17",
+    answer: "4.55mm",
+    options: [
+      "5.05mm",
+      "5.00mm",
+      "4.55mm",
+      "4.05mm"
+    ]
+  },
+     {
+    numb: 18,
+    question: "Which is used to measure the size of the conductor?",
+   
+    answer:"Standard wire gauge",
+    options: [
+      "Bevel gauge",
+      "Vernier caliper",
+      "Standard wire gauge",
+      "Depth gauge"
+    ]
+  },
+       {
+    numb: 19,
+    question: "What is the purpose of serving layer in underground cable?",
+    
+    answer:"Protect armouring from atmospheric condition",
+    options: [
+      "Protect the cable from mechanical vibration",
+      "Protect the cable from mechanical injury",
+      "Protect metallic sheath against corrosion",
+      "Protect armouring from atmospheric condition"
+    ]
+  },
+        {
+    numb: 20,
+    question: "Which type of joint is used for extending the length of conductor in over head lines?",
+   
+    answer:"Western Union joint",
+    options: [
+      "Scarfed joint",
+      "Aerial tap joint",
+      "Britannia T joint",
+      "Western Union joint"
+    ]
+  },  
+     {
+    numb: 21,
+    question: "What is the effect on molten solder due to repeated melting?",
+    
+    answer:"Tin content reduced",
+    options: [
+      "Tin content reduced",
+      "Lead content reduced",
+      "Prevent slug formation",
+      "Even solder flowing in joint"
+    ]
+  },
+                 
+        {
+    numb: 22,
+    question: "Which type of soldering method is used for servicing and repairing of electronics items?",
+    
+    answer:"Soldering with soldering gun",
+    options: [
+      "Dip soldering",
+      "Soldering with a flame",
+      "Soldering with soldering gun",
+      "Machine soldering"
+    ]
+  },  
+     {
+    numb: 23,
+    question: "Which method of soldering is used for repairing the vehicle body?",
+    
+    answer:"Soldering with flame",
+    options: [
+      "Dip soldering",
+      "Soldering with flame",
+      "soldring with soldering iron",
+      "machine soldering"
+    ]
+  }, 
+     {
+    numb: 24,
+    question: "What is the disadvantage of solid conductor compared to stranded conductor?",
+    
+    answer: "Less flexibility",
+    options: [
+      "Less rigidity",
+      "Less flexibility",
+      "Low melting point",
+     "low maechanical strength"
+    ]
+  },
+  {
+    numb: 25,
+    question: "Why the soldering iron must be kept into a stand that not in use while soldering?",
+    
+    answer: "It prevents burns and fire",
+    options: [
+      "It prevents burns and fire",
+      "To control the excessive heat",
+      "To save the time of soldering process",
+     "To avoid dry soldering"
+    ]
+  },        
+               {
+    numb: 26,
+    question: "What is the cause for cold solder defect in soldering?",
+    
+    answer:"Insufficient heating",
+    options: [
+      "Excessive heating",
+      "Insufficient heating",
+      "Incorrect usage of flux",
+      "High wattage soldering iron"
+    ]
+  }, 
+     {
+    numb: 27,
+    question: "What is the value of current (I) flowing through the circuit?",
+       immg:"img27",
+    answer: "2 A",
+    options: [
+      "6 A",
+      "3 A",
+      "2 A",
+     "1 A"
+    ]
+  },
+          {
+    numb: 28,
+    question: "What is the purpose of additional covering over the insulation of insulated conductor?",
+  
+    answer: "To increase dielectric strength",
+    options: [
+      "To increase dielectric strength",
+      "To reduce the conductor resistance",
+      "To reduce the power loss",
+     "To increase the voltage drop"
+    ]
+  },
+
+      {
+    numb: 29,
+    question: "Which type of cables are used for underground services from 33 kv to 66 kv?",
+    
+    answer: "Extra- high tension cables",
+    options: [
+      "High tension cables",
+      "Extra super voltage cables",
+      "Super tension cables",
+     "Extra- high tension cables"
+    ]
+  },
+      {
+    numb: 30,
+    question: "Which is the property of a good conductor?",
+    
+    answer: "Low specific resistance",
+    options: [
+      "Low specific resistance",
+      "High dielectric strength",
+      "High specific resistance",
+     "Low mechanical strength"
+    ]
+  },
+ {
+    numb: 31,
+    question: "Which material is used as conductors for wiring?",
+    
+    answer: "Copper",
+    options: [
+      "Mild Steel",
+      "Copper",
+      "Silver",
+     "Galvanized iron"
+    ]
+  },
+      {
+    numb: 32,
+    question: "Which layer is provided above the metallic sheath in UG cable?",
+    
+    answer: "Bedding",
+    options: [
+      "Armouring",
+      "Bedding",
+      "Serving",
+     "Paper insulation"
+    ]
+  },
+     {
+    numb: 33,
+    question: "What is the formula to find the Electro Motive Force (EMF)?",
+    
+    answer: "EMF = Potential difference + voltage drop",
+    options: [
+      "EMF = Potential difference – voltage drop",
+      "EMF = Potential difference + voltage drop",
+      "EMF = Potential difference + voltage drop/2",
+     "EMF = Potential difference + 2 x voltage drop"
+    ]
+  },
+      {
+    numb: 34,
+    question: "Which law states that the applied voltage in a closed circuit, is equal to the sum of the voltage drops?",
+    
+    answer: "Lead sheath",
+  
+    options: [
+      "Ohms law",
+      "Laws of resistance",
+      "Kirchhoffs first law",
+     "Kirchhoffs second law"
+    ]
+  },
+     {
+    numb: 35,
+    question: "What is the formula for the equivalent resistance (RT) of the three resistors R1, R2 & R3 are connected in parallel?",
+    
+    answer: "RT=1/R1+1/R2+1/R3",
+ 
+    options: [
+      "RT = R1 + R2 + R3",
+      "RT=1/R1+1/R2+1/R3",
+      "RT = 1/(R1 + R2 + R3)",
+     "none"
+    ]
+  },
+     {
+    numb: 36,
+    question: "What is the reading of the ohmmeter connected across the opened R2 resistor as shown in the circuit?",
+     immg:"img36",
+    answer: "Indicate infinite resistance",
+    
+    options: [
+      "Indicate zero reading",
+      "Indicate infinite resistance",
+      "Difference in the value of the resistance of R1 and R3 only",
+     "Sum of the resistance of R1 and R3 only"
+    ]
+  },
+     {
+    numb: 37,
+    question: "What is the name of the part marked X of the belted U.G cable as shown in the figure?",
+    
+    answer: "Wire wound resistor",
+    immg:"img37",
+    options: [
+      "Metal film resistor",
+      "Wire wound resistor",
+      "Carbon -film resistor",
+     "Carbon composition resistor"
+    ]
+  },
+     {
+    numb: 38,
+    question: "Which material is having the negative temperature co-efficient property?",
+    
+    answer: "Mica",
+ 
+    options: [
+      "Mica",
+      "Eureka",
+      "Copper",
+     "Manganin"
+    ]
+  },
+      {
+    numb: 39,
+    question: "Convert 0.456 decimal fraction into percentage?",
+    
+    answer: "45.6%",
+ 
+    options: [
+      "45.6%",
+      "4.56%",
+      "0.456%",
+     "0.0456%"
+    ]
+  },
+      {
+    numb: 40,
+    question: "What is the square of 0.01?",
+    
+    answer: "0.0001",
+ 
+    options: [
+      "0.001",
+      "0.0001",
+      "0.01",
+     "0.00001"
+    ]
+  },
+       {
+    numb: 41,
+    question: "Which metal contains iron as a major content?",
+    
+    answer: "Ferrous metal",
+ 
+    options: [
+      "Brass metal",
+      "bronze metal",
+      "zinc",
+     "Ferrous metal"
+    ]
+  },
+
+       {
+    numb: 42,
+    question: "What is termed as the quantity of matter contained in a body?",
+    
+    answer: "Mass",
+ 
+    options: [
+      "Density",
+      "Volume",
+      "Mass",
+     "Spefific gravity"
+    ]
+  },
+      {
+    numb: 43,
+    question: "What is called mass per unit volume of a substances?",
+    
+    answer: "Denisty",
+ 
+    options: [
+      "Mass",
+      "Weight",
+      "Density",
+     "Volume"
+    ]
+  },
+    {
+    numb: 44,
+    question: "What is the S.I unit of heat?",
+    
+    answer: "Joule",
+ 
+    options: [
+      "Calorie",
+      "Joule",
+      "Centigrade heat unit",
+     "British thermal unit"
+    ]
+  },
+     {
+    numb: 45,
+    question: "Identify the conventional symbol of material?",
+    immg:"img45",
+    answer: "Steel",
+ 
+    options: [
+      "Lead",
+      "Steel",
+      "Glass",
+     "Porcelain"
+    ]
+  },
+     {
+    numb: 46,
+    question: "Identify the conventional symbol of material?",
+    immg:"img46",
+    answer: "Glass",
+ 
+    options: [
+      "Concrete",
+      "Steel",
+      "Wood",
+     "Glass"
+    ]
+  },
+     {
+    numb: 47,
+   question: "Identify the conventional symbol of material?",
+    immg:"img47",
+    answer: "Concrete",
+ 
+    options: [
+      "Steel",
+      "Wood",
+      "Glass",
+     "Concrete"
+    ]
+  },
+      {
+    numb: 48,
+    question: "What is the trimmed size of A4 sheet?",
+ 
+    answer: "210 x 297",
+ 
+    options: [
+      "841 x 1189",
+      "594 x 841",
+      "210 x 297",
+     "148 x 210"
+    ]
+  },
+      {
+    numb: 49,
+    question: "The general form of linear equation",
+    
+    answer: "ax+b",
+ 
+    options: [
+      "ax+b",
+      "b+ax",
+      "a+aax",
+     "bx+ax"
+    ]
+  },
+      {
+    numb: 50,
+    question: "Identify the name of prism?",
+     immg:"img50",
+    answer: "Hexagonal prism",
+ 
+    options: [
+      "Square prism",
+      "Pentagonal prism",
+      "Triangular prism",
+     "Hexagonal prism"
+    ]
+  },
+
+
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
